e959f4442cb76e13c2ffff7a1e4814b772abe8a4
Amit <ambedkaramit176@gmail.com>
2024-06-21 00:04:14 +0530

    add projects

--- src/Components/projects/Project.jsx
+++ src/Components/projects/Project.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { restaurantsite, blogsite, portfolio, chatapp } from "../../assets";
+import {
+  restaurantsite,
+  blogsite,
+  portfolio,
+  chatapp,
+  product_design,
+  blog_app,
+} from "../../assets";
 
-const Project = ({ title, description, image, url }) => {
+const Project = ({ title, description, image, url, liveUrl }) => {
   return (
     <div className="project-card bg-white rounded-lg shadow-md overflow-hidden dark:bg-slate-600 dark:text-stone-100 m-8">
       {image && (
@@ -11,14 +18,24 @@ const Project = ({ title, description, image, url }) => {
         <h3 className="text-xl font-bold">{title}</h3>
         <p className="text-gray-700 mb-3 dark:text-stone-100">{description}</p>
         {url && (
-          <a
-            href={url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:text-blue-700 underline dark:text-stone-100 dark:hover:text-blue-400"
-          >
-            View Project
-          </a>
+          <div className="flex gap-2">
+            <a
+              href={liveUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:text-blue-700 underline dark:text-stone-100 dark:hover:text-blue-400"
+            >
+              Preview
+            </a>
+            <a
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:text-blue-700 underline dark:text-stone-100 dark:hover:text-blue-400"
+            >
+              Github Code
+            </a>
+          </div>
         )}
       </div>
     </div>
@@ -28,33 +45,52 @@ const Project = ({ title, description, image, url }) => {
 const Projects = () => {
   const projects = [
     {
+      title: "Blog Application",
+      description: `Developed a blog web application leveraging Node.js and Express.js. The front end was ·	The application features a comprehensive backend that supports reading and writing blog posts. Additionally, it includes a user authentication system that allows signed-in users to comment on blogs`,
+      image: blog_app,
+      url: "https://github.com/Am4874/Chat_Application",
+      liveUrl: "https://blog-application-mfov.onrender.com/",
+    },
+    {
       title: "Chat Application",
-      description: `Developed a simple web chat application using React JS as a front end and Node JS, Express JS as a back end Socket.io for real
-      time chat communication channel between a client and a server.
-      In this application we can create a room with room id and chat with multiple users at same time in chat room.`,
+      description: `Developed a straightforward web chat application utilizing React JS for the front end and Node JS, Express JS for the backend. Socket.io was implemented to facilitate real-time chat communication between the client and the server.
+      ·	This application has the capability to create a chat room using a room ID, allowing multiple users to engage in simultaneous conversations within the same chat room.`,
       image: chatapp,
       url: "https://github.com/Am4874/Chat_Application",
+      liveUrl: "https://github.com/Am4874/Chat_Application",
     },
     {
       title: "Portfolio Website|",
       description: `Developed a portfolio website using React JS as front end and styling using CSS.
       This portfolio website to show an information and skills of user and contact information of user.`,
       image: portfolio,
-      url: "https://portfoliio-6m07.onrender.com/",
+      url: "https://github.com/Am4874/portfolio",
+      liveUrl: "https://portfoliio-6m07.onrender.com/",
+    },
+    {
+      title: "Product Designer Landing Page ",
+      description: `Constructed a Product Designer Landing Page using JavaScript,CSS,HTML. Animated design using Locomotive Design.`,
+      image: product_design,
+      url: "https://github.com/Am4874/Product-Deigner",
+      liveUrl: "https://am4874.github.io/Product-Deigner/",
     },
     {
       title: "Personal Blog Website",
-      description: `Developed a Personal Blog Website using JavaScript for behaviors and Styling using CSS and Html for struct of site.
-      This frontend website to a post personal blog and interest.`,
+      description: `Constructed a portfolio website utilizing React JS for the front end, with styling accomplished through CSS
+      This portfolio website serves as a platform to showcase the user’s skills and information, as well as providing a means to contact the user.
+`,
       image: blogsite,
-      url: "https://am4874.github.io/personal_blogs_website/",
+      url: "https://github.com/Am4874/personal_blogs_website",
+      liveUrl: "https://am4874.github.io/personal_blogs_website/",
     },
     {
       title: "Online Restaurant Site",
-      description: `Developed an Online restaurant site using JavaScript, CSS and HTML.In this frontend website we can Book a table, see a menu and order a food in restaurant and review a food also see a chef
-      who preparing a food.`,
+      description: `I developed an online restaurant website using JavaScript, CSS, and HTML.
+      This front-end website offers features such as table booking, viewing the menu, and ordering food. It also provides the ability to review dishes and see the chefs who prepare the food.
+`,
       image: restaurantsite,
-      url: "https://am4874.github.io/Restaurant_website/",
+      url: "https://github.com/Am4874/Restaurant_website",
+      liveUrl: "https://am4874.github.io/Restaurant_website/",
     },
   ];
 
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,48 +1,51 @@
-import bootstrap from './image/bootstrap.png';
-import css3 from './image/css-3.png';
-import expressjs from './image/express-js.png';
-import github from './image/github.png';
-import gmail from './image/gmail.png';
-import html from './image/html.png';
-import instagram from './image/instagram.png';
-import javascript from './image/javascript.png';
+import bootstrap from "./image/bootstrap.png";
+import css3 from "./image/css-3.png";
+import expressjs from "./image/express-js.png";
+import github from "./image/github.png";
+import gmail from "./image/gmail.png";
+import html from "./image/html.png";
+import instagram from "./image/instagram.png";
+import javascript from "./image/javascript.png";
 import js from "./image/js.png";
 import linkedin from "./image/linkedin.png";
-import nodeJs from './image/nodejs.png';
-import python from './image/python.png';
-import react from './image/react.png';
-import react2 from './image/react2.png';
-import sql from './image/sql.png';
-import tailwindcss from './image/tailwindcss.png';
-import X from './image/X.png';
-import mail from './image/mail.png'
-import portfolio from './image/portfolio.jpg'
-import restaurantsite from './image/restaurantsite.jpg'
-import blogsite from './image/blogsite.jpg'
-import chatapp from './image/chatapp.png'
-
+import nodeJs from "./image/nodejs.png";
+import python from "./image/python.png";
+import react from "./image/react.png";
+import react2 from "./image/react2.png";
+import sql from "./image/sql.png";
+import tailwindcss from "./image/tailwindcss.png";
+import X from "./image/X.png";
+import mail from "./image/mail.png";
+import portfolio from "./image/portfolio.jpg";
+import restaurantsite from "./image/restaurantsite.jpg";
+import blogsite from "./image/blogsite.jpg";
+import chatapp from "./image/chatapp.png";
+import product_design from "./image/product_design.jpg";
+import blog_app from "./image/blog_app.jpg";
 
 export {
-    bootstrap,
-    github,
-    javascript,
-    js,
-    linkedin,
-    nodeJs,
-    expressjs,
-    python,
-    react,
-    react2,
-    sql,
-    tailwindcss,
-    X,
-    instagram,
-    gmail,
-    html,
-    css3,
-    mail,
-    portfolio,
-    restaurantsite,
-    blogsite,
-    chatapp,
+  bootstrap,
+  github,
+  javascript,
+  js,
+  linkedin,
+  nodeJs,
+  expressjs,
+  python,
+  react,
+  react2,
+  sql,
+  tailwindcss,
+  X,
+  instagram,
+  gmail,
+  html,
+  css3,
+  mail,
+  portfolio,
+  restaurantsite,
+  blogsite,
+  chatapp,
+  product_design,
+  blog_app,
 };
